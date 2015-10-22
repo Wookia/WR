ec1cbb9436393dc8fc223a64e62f58fcf9b69ac9
Wookia <lukasz@butryn.eu>
2015-10-22 14:44:52 +0200

    Creating classes
    Make code better for future use (creating classes for motor and params)

--- main.py
+++ main.py
@@ -2,55 +2,54 @@
 
 import time
 from ev3dev import *
+class EV3:
+    def __init__(self):
+        self.lmotor = large_motor(OUTPUT_C); assert lmotor.connected
+        self.rmotor = large_motor(OUTPUT_B); assert rmotor.connected
+        self.ts = touch_sensor(); assert ts.connected
+        self.cs = color_sensor(); assert cs.connected
+        self.ls = light_sensor(); assert ls.connected
+        self.lmotor.speed_regulation_enabled = 'on'
+        self.rmotor.speed_regulation_enabled = 'on'
 
-lmotor = large_motor(OUTPUT_C); assert lmotor.connected
-rmotor = large_motor(OUTPUT_B); assert rmotor.connected
-ts = touch_sensor(); assert ts.connected
-cs = color_sensor(); assert cs.connected
-ls = light_sensor(); assert ls.connected
-lmotor.speed_regulation_enabled = 'on'
-rmotor.speed_regulation_enabled = 'on'
+class Params:
+    def __init__(self):
+        self.whiteLS = ev3.ls.value()
+        self.whiteCS = ev3.cs.value()
+        self.blackLS = ev3.ls.value()
+        self.blackCS = ev3.cs.value()
+    def calibrate(EV3, time):
+        self.scan(EV3, time, -1)
+        self.scan(EV3, time, 1)
+        self.scan(EV3, time, 1)
+        self.scan(EV3, time, -1)
+    def scan(EV3, time, dir):
+        i = 0
+        while (i < time):
+            EV3.lmotor.run_forever(speed_sp=dir*100)
+            EV3.rmotor.run_forever(speed_sp=-dir*100)
+            valLS = EV3.ls.value()
+            valCS = EV3.cs.value()
+            if (valLS > whiteLS):
+                whiteLS = valLS
+            elif (valLS < blackLS):
+                blackLS = valLS
 
-def scan(time, dir):
-    global whiteLS
-    global whiteCS
-    global blackLS
-    global blackCS
-    global lmotor
-    global rmotor
-    global ls
-    global cs
-    i = 0
-    while (i < time):
-        lmotor.run_forever(speed_sp=dir*100)
-        rmotor.run_forever(speed_sp=-dir*100)
-        valLS = ls.value()
-        valCS = cs.value()
-        if (valLS > whiteLS):
-            whiteLS = ls.value()
-        elif (valLS < blackLS):
-            blackLS = ls.value()
-
-        if (valCS > whiteCS):
-            whiteCS = cs.value()
-        elif (valCS < blackCS):
-            blackCS = cs.value()
-        i+=1
-    lmotor.stop()
-    rmotor.stop()
+            if (valCS > whiteCS):
+                whiteCS = valCS
+            elif (valCS < blackCS):
+                blackCS = valCS
+            i+=1
+        lmotor.stop()
+        rmotor.stop()
 
 
-while not ts.value():
+EV3 = ev3()
+Params = Params()
+while not EV3.ts.value():
     print("HWDP")
-whiteLS = ls.value() 
-whiteCS = cs.value()
-blackLS = ls.value() 
-blackCS = cs.value() 
-scan(250, -1)
-scan(250, 1)
-scan(250, 1)
-scan(250, -1)
-print("WhiteLS: %s BlackLS: %s" %(whiteLS, blackLS))
-print("WhiteCS: %s BlackCS: %s" %(whiteCS, blackCS))
-while not ts.value():
-	time.sleep(0.1)	
+Params.calibrate(EV3, 250)
+print("WhiteLS: %s BlackLS: %s" %(Params.whiteLS, Params.blackLS))
+print("WhiteCS: %s BlackCS: %s" %(Params.whiteCS, Params.blackCS))
+while not EV3.ts.value():
+	time.sleep(0.1)
