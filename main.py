ccb68105961e282295b657ca14d7af99397f7925
sadaharu-inugami <radoslawbialobrzeski@gmail.com>
2015-10-22 15:03:34 +0200

    fixes fixes fixes

--- main.py
+++ main.py
@@ -4,48 +4,48 @@ import time
 from ev3dev import *
 class EV3:
     def __init__(self):
-        self.lmotor = large_motor(OUTPUT_C); assert lmotor.connected
-        self.rmotor = large_motor(OUTPUT_B); assert rmotor.connected
-        self.ts = touch_sensor(); assert ts.connected
-        self.cs = color_sensor(); assert cs.connected
-        self.ls = light_sensor(); assert ls.connected
+        self.lmotor = large_motor(OUTPUT_C); assert self.lmotor.connected
+        self.rmotor = large_motor(OUTPUT_B); assert self.rmotor.connected
+        self.ts = touch_sensor(); assert self.ts.connected
+        self.cs = color_sensor(); assert self.cs.connected
+        self.ls = light_sensor(); assert self.ls.connected
         self.lmotor.speed_regulation_enabled = 'on'
         self.rmotor.speed_regulation_enabled = 'on'
 
 class Params:
-    def __init__(self):
-        self.whiteLS = ev3.ls.value()
-        self.whiteCS = ev3.cs.value()
-        self.blackLS = ev3.ls.value()
-        self.blackCS = ev3.cs.value()
-    def calibrate(EV3, time):
+    def __init__(self, EV3):
+        self.whiteLS = EV3.ls.value()
+        self.whiteCS = EV3.cs.value()
+        self.blackLS = EV3.ls.value()
+        self.blackCS = EV3.cs.value()
+    def calibrate(self, EV3, time):
         self.scan(EV3, time, -1)
         self.scan(EV3, time, 1)
         self.scan(EV3, time, 1)
         self.scan(EV3, time, -1)
-    def scan(EV3, time, dir):
+    def scan(self, EV3, time, dir):
         i = 0
         while (i < time):
             EV3.lmotor.run_forever(speed_sp=dir*100)
             EV3.rmotor.run_forever(speed_sp=-dir*100)
             valLS = EV3.ls.value()
             valCS = EV3.cs.value()
-            if (valLS > whiteLS):
-                whiteLS = valLS
-            elif (valLS < blackLS):
-                blackLS = valLS
+            if (valLS > self.whiteLS):
+                self.whiteLS = valLS
+            elif (valLS < self.blackLS):
+                self.blackLS = valLS
 
-            if (valCS > whiteCS):
-                whiteCS = valCS
-            elif (valCS < blackCS):
-                blackCS = valCS
+            if (valCS > self.whiteCS):
+                self.whiteCS = valCS
+            elif (valCS < self.blackCS):
+                self.blackCS = valCS
             i+=1
-        lmotor.stop()
-        rmotor.stop()
+        EV3.lmotor.stop()
+        EV3.rmotor.stop()
 
 
-EV3 = ev3()
-Params = Params()
+EV3 = EV3()
+Params = Params(EV3)
 while not EV3.ts.value():
     print("HWDP")
 Params.calibrate(EV3, 250)
