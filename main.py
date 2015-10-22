4a1fcb8775606e115c375c67d8656dba37aaa114
Wookia <lukasz@butryn.eu>
2015-10-22 13:41:43 +0200

    Calibration
    First try of color calibration

--- main.py
+++ main.py
@@ -5,12 +5,34 @@ from ev3dev import *
 
 lmotor = large_motor(OUTPUT_C); assert lmotor.connected
 rmotor = large_motor(OUTPUT_B); assert rmotor.connected
-irsens = infrared_sensor();     assert irsens.connected
+ts = touch_sensor(); assert ts.connected
+cs = color_sensor(); assert cs.connected
+ls = light_sensor(); assert ls.connected
+lmotor.speed_regulation_enabled = 'on'
+rmotor.speed_regulation_enabled = 'on'
+whiteLS = None
+whiteCS = None
+blackLS = None
+blackCS = None
 
-rc = remote_control(irsens)
+def scan(time, dir):
+    i = 0
+    while (i < time):
+        action_button(lmotor, dir*50)
+        action_button(rmotor, -dir*50)
+        valLS = ls.value()
+        valCS = cs.value()
+        if (valLS > whiteLS)
+            whiteLS = ls.value()
+        elif (valLS < blackLS)
+            blackLS = ls.value()
 
-lmotor.set(speed_regulation_enabled='off', stop_command='brake')
-rmotor.set(speed_regulation_enabled='off', stop_command='brake')
+        if (valCS > whiteCS)
+            whiteCS = cs.value()
+        elif (valCS < blackCS)
+            blackCS = cs.value()
+        i++
+
 
 def action_button(motor, pwm):
     def roll(state):
@@ -19,12 +41,12 @@ def action_button(motor, pwm):
         else:
             motor.stop()
     return roll
-
-rc.on_red_up   (action_button(lmotor,  100))
-rc.on_red_down (action_button(lmotor, -100))
-rc.on_blue_up  (action_button(rmotor,  100))
-rc.on_blue_down(action_button(rmotor, -100))
-
-while True:
-    rc.process()
-    sleep(0.01)
+while not ts.value():
+    print("HWDP")
+scan(1000, -1)
+scan(1000, 1)
+scan(1000, 1)
+scan(1000, -1)
+while not ts.value():
+    print("WhiteLS: %s BlackLS: %s" %(whiteLS, blackLS))
+    print("WhiteCS: %s BlackCS: %s" %(whiteCS, blackCS))
